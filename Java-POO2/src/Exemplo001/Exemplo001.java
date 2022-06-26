6b3f3386b9d6f14fbd950ed997e7d47cc4541692
LucasPinheiro <lucas.rpinheiro@mercadolivre.com>
2022-06-26 18:17:54 -0300

    feat: inicia pratica integradora 01

---
+++ Java-POO2/src/Exemplo001/Exemplo001.java
@@ -0,0 +1,9 @@
+package Exemplo001;
+
+public class Exemplo001 {
+    public static void main(String[] args) {
+        Temperaturas cidades = new Temperaturas();
+
+        cidades.getCidades();
+    }
+}
---
+++ Java-POO2/src/Exemplo001/Temperaturas.java
@@ -0,0 +1,69 @@
+package Exemplo001;
+
+public class Temperaturas {
+    String cidades[] = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};;
+    int temperaturas[][] = new int [10][2];
+    private int temperaturaMinima = 0;
+    private int temperatraMaxima = 1;
+
+    Temperaturas() {
+        this.insertIntoTemperaturas();
+    }
+
+    public void getCidades() {
+        for (int index = 0; index < cidades.length; index += 1) {
+            System.out.println(cidades[index]);
+            System.out.println("Temperatura Mínima: "
+                    + temperaturas[index][temperaturaMinima]
+                    + " Temperatura Máxima: "
+                    + temperaturas[index][temperatraMaxima]);
+        }
+    }
+
+    private void insertIntoTemperaturas() {
+        for (int linha = 0; linha < temperaturas.length; linha += 1) {
+            switch (linha) {
+                case 0:
+                    temperaturas[linha][temperaturaMinima] = -2;
+                    temperaturas[linha][temperatraMaxima] = 33;
+                    break;
+                case 1:
+                    temperaturas[linha][temperaturaMinima] = -3;
+                    temperaturas[linha][temperatraMaxima] = 32;
+                    break;
+                case 2:
+                    temperaturas[linha][temperaturaMinima] = -8;
+                    temperaturas[linha][temperatraMaxima] = 27;
+                    break;
+                case 3:
+                    temperaturas[linha][temperaturaMinima] = 4;
+                    temperaturas[linha][temperatraMaxima] = 37;
+                    break;
+                case 4:
+                    temperaturas[linha][temperaturaMinima] = 6;
+                    temperaturas[linha][temperatraMaxima] = 42;
+                    break;
+                case 5:
+                    temperaturas[linha][temperaturaMinima] = 5;
+                    temperaturas[linha][temperatraMaxima] = 43;
+                    break;
+                case 6:
+                    temperaturas[linha][temperaturaMinima] = 0;
+                    temperaturas[linha][temperatraMaxima] = 39;
+                    break;
+                case 7:
+                    temperaturas[linha][temperaturaMinima] = -7;
+                    temperaturas[linha][temperatraMaxima] = 26;
+                    break;
+                case 8:
+                    temperaturas[linha][temperaturaMinima] = -1;
+                    temperaturas[linha][temperatraMaxima] = 31;
+                    break;
+                case 9:
+                    temperaturas[linha][temperaturaMinima] = -10;
+                    temperaturas[linha][temperatraMaxima] = 35;
+                    break;
+            }
+        }
+    }
+}
